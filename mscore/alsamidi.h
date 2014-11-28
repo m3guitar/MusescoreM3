musescore/MuseScore

Automatic control of JACK/ALSA MIDI Out ports

mscore/alsamidi.h
@@ -56,6 +56,7 @@ class AlsaMidiDriver : public MidiDriver {
       virtual void getOutputPollFd(struct pollfd**, int* n);
       virtual void read();
       virtual void write(const Event&);
+      virtual void updateInPortNumber(int);
       };
 
 }

mscore/driver.h
@@ -53,6 +53,7 @@ class Driver {
       virtual void handleTimeSigTempoChanged() {}
       virtual void checkTransportSeek(int, int, bool) {}
       virtual int bufferSize() {return 0;}
+      virtual void updateOutPortNumber(int) {}
       };
 
 

m3guitar/MusescoreM3

Former-commit-id: 6bc6742a65792d682d80285cf7e2127931a550fe

mscore/alsamidi.h
@@ -56,6 +56,7 @@ class AlsaMidiDriver : public MidiDriver {
       virtual void getOutputPollFd(struct pollfd**, int* n);
       virtual void read();
       virtual void write(const Event&);
+      virtual void updateInPortCount(int);
       };
 
 }

mscore/driver.h
@@ -53,6 +53,7 @@ class Driver {
       virtual void handleTimeSigTempoChanged() {}
       virtual void checkTransportSeek(int, int, bool) {}
       virtual int bufferSize() {return 0;}
+      virtual void updateOutPortCount(int) {}
       };
 
 